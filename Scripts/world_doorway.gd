fefff717787345e8a1d44179e1163fbb6413db17
Ariel Valdez <arielvaldez017@outlook.com>
2024-01-27 12:12:04 -0400

    inicializando mecánica de puertas. Se necesita cambiar la construcción del script

--- Scripts/world_doorway.gd
+++ Scripts/world_doorway.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 @export var OtherSide : Node2D
+var ThisSide : Node2D
 
 func _ready() -> void:
 	if !OtherSide:
 		OtherSide = $"."
-	pass 
+	pass
+	
+	ThisSide = get_tree().current_scene
 
 func CrossDoorForNextWorld() -> void:
 	# play animation for enter door
